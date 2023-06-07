e6355a91d80c1f316aea984717960f9974b4568a
Antonio Pitasi <antonio.pitasi@qredo.com>
2023-06-07 18:22:20 +0200

    try codeium

--- plugins/user.lua
+++ plugins/user.lua
@@ -16,7 +16,30 @@ return {
 
   {
     "akinsho/git-conflict.nvim",
-    config = function() require('git-conflict').setup() end,
+    config = function() require('git-conflict').setup({}) end,
     lazy = false
   },
+
+  {
+    "ggandor/lightspeed.nvim",
+    lazy = false
+  },
+
+  {
+    'Exafunction/codeium.vim',
+    lazy = false,
+    config = function()
+      vim.keymap.set("i", "<C-l>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
+      vim.keymap.set("i", "<c-;>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true })
+      vim.keymap.set("i", "<c-,>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
+      vim.keymap.set("i", "<c-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
+      vim.keymap.set("n", "<leader>;", function()
+        if vim.g.codeium_enabled == true then
+          vim.cmd "CodeiumDisable"
+        else
+          vim.cmd "CodeiumEnable"
+        end
+      end, { noremap = true, desc = "Toggle Codeium active" })
+    end,
+  }
 }
